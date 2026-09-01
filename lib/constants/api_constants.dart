796dcf62e90c9c186e98a6457dd8b71ef812beff
Ali Al-Qoraan <aliayalquraan8@gmail.com>
2026-09-01 18:26:21 +0300

    Migrate ApiConstants to JavaScript object format

--- lib/constants/api_constants.dart
+++ lib/constants/api_constants.dart
@@ -1,17 +1,7 @@
-/// API Configuration and Constants for Jordanian SER backend
-class ApiConstants {
-  ApiConstants._();
-
-  /// The production backend domain
-  static const String baseUrl = 'https://serios.eqratech.com';
-
-  /// Endpoint paths
-  static const String submitAudioEndpoint = '/api/submit-audio';
-  static const String healthCheckEndpoint = '/';
-
-  /// Request timeouts
-  static const Duration uploadTimeout = Duration(seconds: 45);
-
-  /// Default headers
-  static const String userAgent = 'JordanianSERApp/Flutter';
-}
+export const ApiConstants = Object.freeze({
+  baseUrl: 'https://serios.eqratech.com',
+  submitAudioEndpoint: '/api/submit-audio',
+  healthCheckEndpoint: '/',
+  uploadTimeout: 45000,
+  userAgent: 'JordanianSERApp/Flutter',
+});
