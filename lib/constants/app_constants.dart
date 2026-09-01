7cd070577379205d74153ae8a7c79843aee95659
Ali Al-Qoraan <aliayalquraan8@gmail.com>
2026-09-01 18:26:46 +0300

    Refactor AppConstants to use Object.freeze

--- lib/constants/app_constants.dart
+++ lib/constants/app_constants.dart
@@ -1,12 +1,8 @@
-/// General App-wide constants
-class AppConstants {
-  AppConstants._();
+export const AppConstants = Object.freeze({
+  appName: 'تحدي الصوت الأردني',
+  appVersion: '1.0.0',
 
-  static const String appName = 'تحدي الصوت الأردني';
-  static const String appVersion = '1.0.0';
-
-  // Audio recording settings
-  static const int sampleRate = 16000;
-  static const int numChannels = 1;
-  static const int bitRate = 256000;
-}
+  sampleRate: 16000,
+  numChannels: 1,
+  bitRate: 256000,
+});
