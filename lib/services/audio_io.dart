abce6bb05dc4f3fb3aaa432a4f8ea9cc664bbb25
Ali Al-Qoraan <aliayalquraan8@gmail.com>
2026-09-01 18:31:39 +0300

    Refactor audio_io.dart to use dynamic imports

--- lib/services/audio_io.dart
+++ lib/services/audio_io.dart
@@ -1,4 +1,14 @@
-export 'audio_io_stub.dart'
-    if (dart.library.io) 'audio_io_native.dart'
-    if (dart.library.html) 'audio_io_web.dart'
-    if (dart.library.js_interop) 'audio_io_web.dart';
+const isWeb = typeof window !== 'undefined' && typeof document !== 'undefined';
+const isNative = typeof process !== 'undefined' && process.versions && process.versions.node;
+
+let audioModule;
+
+if (isWeb) {
+  audioModule = require('./audio_io_web');
+} else if (isNative) {
+  audioModule = require('./audio_io_native');
+} else {
+  audioModule = require('./audio_io_stub');
+}
+
+module.exports = audioModule;
